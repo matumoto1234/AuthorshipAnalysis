ac349143a61c0b76292db3e7577a7ff988a4250a
matumoto1234 <matumotosizuoka@gmail.com>
2023-05-27 03:59:37 +0900

    main.pyの関数を呼び出せるように修正

--- finalproject/main.py
+++ finalproject/main.py
@@ -2,29 +2,25 @@ import pandas
 
 # ここでcsvを読み込んで、pandasのDataFrame型にする
 # 必要であれば色々前処理をする
-def read_csv() -> pandas.DataFrame:
-    # google drive path
-    csv = pandas.read_csv('/content/drive/MyDrive/UniversityOfAizu/2023-Q1/authorship-analysis-using-python/archive/blogtext.csv', nrows = 10000)
+def read_csv(nrows: int) -> pandas.DataFrame:
+    csv = pandas.read_csv('/content/drive/MyDrive/UniversityOfAizu/2023-Q1/authorship-analysis-using-python/archive/blogtext.csv', nrows=nrows)
     return csv
 
 
-def main():
-    csv = read_csv()
-
-    # male_authors, female_authorsの構築
-    male_authors: list[str] = []
-    female_authors: list[str] = []
+def extract_authors(csv: pandas.DataFrame, target_gender: str) -> list[str]:
+    authors: list[str] = []
 
     for i in range(len(csv)):
         author = csv[i:i+1]['sign'].values[0]
         gender = csv[i:i+1]['gender'].values[0]
 
-        if gender == 'male':
-            male_authors.append(author)
-        elif gender == 'female':
-            female_authors.append(author)
+        if gender == target_gender:
+            authors.append(author)
 
-    # author_to_textsの構築
+    return authors
+
+
+def make_author_to_texts(csv: pandas.DataFrame) -> dict[str, list[list[str]]]:
     author_to_texts: dict[str, list[list[str]]] = {}
 
     for i in range(len(csv)):
@@ -36,8 +32,19 @@ def main():
         else:
             author_to_texts[author] = [text]
 
+    return author_to_texts
+
+
+def main():
+    csv = read_csv(nrows=500)
+
+    male_authors = extract_authors(csv, 'male')
+    female_authors = extract_authors(csv, 'female')
+    author_to_texts = make_author_to_texts(csv)
+
     print(len(male_authors))
     print(len(female_authors))
 
 
-main()
+if __name__ == '__main__':
+    main()
